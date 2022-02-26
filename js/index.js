316d2c8e1d24d7e03ad90287e25e04f48ed20af2
evelin-mart <evalouis666@gmail.com>
2022-02-26 20:50:31 +0300

    feat: add hamburger menu

--- js/index.js
+++ js/index.js
@@ -8,3 +8,27 @@ function reverse() {
 
 const example = document.querySelector(".example");
 example.addEventListener("click", reverse);
+
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
+const body = document.body;
+const menuAddictedElements = [hamburger, nav, body];
+
+function editClass(array, method, cl) {
+  array.forEach((elem) => {
+    elem.classList[method](cl);
+  });
+}
+function toggleClass() {
+  nav.classList.toggle("nav-open");
+  editClass(menuAddictedElements, "toggle", "menu-open");
+}
+hamburger.addEventListener("click", toggleClass);
+
+function deleteClass(event) {
+  if (event.target.classList.contains("menu__link")) {
+    nav.classList.remove("nav-open");
+    editClass(menuAddictedElements, "remove", "menu-open");
+  }
+}
+nav.addEventListener("click", deleteClass);
